4ca716218a00d8ef76ac9201bcfc6bc3f176cfbf
Flávio Rodrigues Leal <flavio.rodrigues.leal@gmail.com>
2024-12-17 22:56:26 -0300

    Import component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+interface IPlanos {
+    infos: IInfos;
+}
+
+interface IInfos {
+    tipo: string;
+    preco: number;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  plano = {
+  //@ts-ignore
+  plano: IPlanos = {
     infos: {
       tipo : 'Simples',
       preco: 100,
